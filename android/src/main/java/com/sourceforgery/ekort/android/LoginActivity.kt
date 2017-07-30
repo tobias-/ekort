4e00aeab2fc95a1bc7f85a4739be3551176f0560
Tobias <tobias@olsson.be>
2017-07-30 10:13:23 +0200

    Progress bar during login

--- android/src/main/java/com/sourceforgery/ekort/android/LoginActivity.kt
+++ android/src/main/java/com/sourceforgery/ekort/android/LoginActivity.kt
@@ -13,12 +13,11 @@ import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
-import com.sourceforgery.swedbank.ECardAPI
-import com.sourceforgery.swedbank.ECardClient
-import com.sourceforgery.swedbank.debugLevel
-import com.sourceforgery.swedbank.logger
+import com.sourceforgery.swedbank.*
 import kotlinx.android.synthetic.main.activity_login.*
 import okhttp3.logging.HttpLoggingInterceptor
+import android.content.DialogInterface
+import android.support.v7.app.AlertDialog
 
 
 /**
@@ -130,34 +129,62 @@ class LoginActivity : AppCompatActivity() {
                 })
     }
 
+    fun updateProgress(status: LoginStatus) {
+        val progressText = getString(when (status) {
+            LoginStatus.NOT_STARTED -> R.string.login_NOT_STARTED
+            LoginStatus.INIT_EKORT_COMPLETE -> R.string.login_INIT_EKORT_COMPLETE
+            LoginStatus.STARTED -> R.string.login_STARTED
+            LoginStatus.PORTAL_INIT_COMPLETE -> R.string.login_PORTAL_INIT_COMPLETE
+            LoginStatus.LOGIN_1_COMPLETE -> R.string.login_LOGIN_1_COMPLETE
+            LoginStatus.LOGIN_2_COMPLETE -> R.string.login_LOGIN_2_COMPLETE
+            LoginStatus.ERROR -> R.string.login_ERROR
+            LoginStatus.STARTED_POLLING -> R.string.login_STARTED_POLLING
+            LoginStatus.POLL_COMPLETE -> R.string.login_POLL_COMPLETE
+            LoginStatus.PRE_CLIENT_COMPLETE -> R.string.login_PRE_CLIENT_COMPLETE
+            LoginStatus.LOGIN_3_COMPLETE -> R.string.login_LOGIN_3_COMPLETE
+            LoginStatus.SELECT_ISSUER_STARTED -> R.string.login_SELECT_ISSUER_STARTED
+            LoginStatus.SELECT_ISSUER_COMPLETE -> R.string.login_SELECT_ISSUER_COMPLETE
+        })
+        login_progress_text.text = progressText
+    }
+
+
     /**
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
      */
     @SuppressLint("StaticFieldLeak")
-    inner class UserLoginTask internal constructor(private val personNumber: String) : AsyncTask<Void, Void, ECardAPI?>() {
+    inner class UserLoginTask internal constructor(private val personNumber: String) : AsyncTask<Void, LoginStatus, List<ECardClient.Account>?>() {
 
         var error: String? = null
 
-        override fun doInBackground(vararg params: Void): ECardAPI? {
+        override fun doInBackground(vararg params: Void): List<ECardClient.Account>? {
             try {
-                debugLevel = HttpLoggingInterceptor.Level.BODY
+                debugLevel = HttpLoggingInterceptor.Level.NONE
                 logger = object : HttpLoggingInterceptor.Logger {
                     override fun log(message: String?) {
                         Log.d("OkHttp3", message)
                     }
                 }
                 val eCardClient = ECardClient(personNumber)
+                eCardClient.statusUpdatedListener = object : StatusUpdateListener {
+                    override fun statusUpdated(status: LoginStatus) {
+                        updateProgress(status)
+                    }
+                }
                 eCardClient.loginWithoutPoll()
                 openBankIdApp()
-                val pollAndGetAccounts = eCardClient.pollAndGetAccounts()
-                return pollAndGetAccounts[0].selectIssuer()
+                return eCardClient.pollAndGetAccounts()
             } catch (e: InterruptedException) {
                 return null
             } catch (e: Exception) {
                 error = e.message
                 return null
             }
+        }
+
+        override fun onProgressUpdate(vararg values: LoginStatus) {
+            updateProgress(values.last())
         }
 
         private fun openBankIdApp() {
@@ -169,10 +196,46 @@ class LoginActivity : AppCompatActivity() {
             startActivityForResult(intent, 0)
         }
 
-        override fun onPostExecute(success: ECardAPI?) {
-            mAuthTask = null
+        override fun onPostExecute(success: List<ECardClient.Account>?) {
             showProgress(false)
 
+            if (success != null) {
+                val accounts = success.map { "${it.personNumber} ${it.bankName} ${it.name}" }.toTypedArray()
+
+                val builder = AlertDialog.Builder(this@LoginActivity)
+                builder.setTitle(getString(R.string.pick_issuer))
+                builder.setCancelable(false)
+                builder.setItems(accounts, DialogInterface.OnClickListener { dialog, which ->
+                    AccountSelectTask().execute(success[which])
+                })
+                builder.show()
+            } else {
+                this@LoginActivity.personNumber.error = error
+                this@LoginActivity.personNumber.requestFocus()
+            }
+            mAuthTask = null
+        }
+
+        override fun onCancelled() {
+            mAuthTask = null
+            showProgress(false)
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    inner class AccountSelectTask internal constructor() : AsyncTask<ECardClient.Account, Void, ECardAPI?>() {
+        private var error: String? = null
+
+        override fun doInBackground(vararg arg: ECardClient.Account?): ECardAPI? {
+            try {
+                return arg[0]!!.selectIssuer()
+            } catch (e: Exception) {
+                this.error = e.message
+                return null
+            }
+        }
+
+        override fun onPostExecute(success: ECardAPI?) {
             if (success != null) {
                 val intent = Intent(this@LoginActivity, EkortActivity::class.java)
                 intent.putExtra("webServletUrl", success.webServletUrl.toString())
@@ -185,11 +248,6 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        override fun onCancelled() {
-            mAuthTask = null
-            showProgress(false)
-        }
-    }
 
-    inner class AccountSelectTask internal constructor()
+    }
 }
