a94e7eed374000c43095109b0db3035270139040
Tobias <tobias@olsson.be>
2017-08-02 23:08:13 +0200

    Remove Settings menu

--- android/src/main/java/com/sourceforgery/ekort/android/EkortActivity.kt
+++ android/src/main/java/com/sourceforgery/ekort/android/EkortActivity.kt
@@ -95,22 +95,6 @@ class EkortActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.ekort, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_past_transactions -> {
