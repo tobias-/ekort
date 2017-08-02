891c3cb37924c3836ce41bbcd62950e7beac6c39
Tobias <tobias@olsson.be>
2017-08-02 23:10:39 +0200

    Add background tasks for loading transactions and active cards

--- android/src/main/java/com/sourceforgery/ekort/android/EkortActivity.kt
+++ android/src/main/java/com/sourceforgery/ekort/android/EkortActivity.kt
@@ -103,13 +103,13 @@ class EkortActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_past_transactions -> {
-
+                LoadPastTransactions().execute()
             }
             R.id.nav_active_cards -> {
-
+                LoadActiveECards().execute()
             }
             R.id.nav_create_card -> {
-                showProgress(create_ecard_form)
+                showScreen(create_ecard_form)
             }
         }
         drawer_layout.closeDrawer(GravityCompat.START)
@@ -117,17 +117,42 @@ class EkortActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
     }
 
     @SuppressLint("StaticFieldLeak")
-    inner class LoadTransactions : AsyncTask<Void, Void, List<PastTransaction>?>() {
+    inner class LoadPastTransactions : AsyncTask<Void, Void, List<PastTransaction>?>() {
+
+        override fun onPreExecute() {
+            showScreen(ekort_progress)
+        }
 
         override fun doInBackground(vararg p0: Void?): List<PastTransaction>? = eCardAPI!!.getPastTransactions(0)
 
 
         override fun onPostExecute(result: List<PastTransaction>?) {
-            showProgress(past_transactions)
             if (result != null) {
-                past_transactions.visibility = View.VISIBLE
-                table.adapter = PastTransactionsAdapter(layoutInflater, result)
-                table.layoutManager = LinearLayoutManager(this@EkortActivity)
+                past_transactions_table.adapter = PastTransactionsAdapter(layoutInflater, result)
+                past_transactions_table.layoutManager = LinearLayoutManager(this@EkortActivity)
+                showScreen(past_transactions)
+            } else {
+                throw RuntimeException("What happened here?")
+            }
+        }
+    }
+
+
+    @SuppressLint("StaticFieldLeak")
+    inner class LoadActiveECards : AsyncTask<Void, Void, List<ActiveECard>?>() {
+
+        override fun onPreExecute() {
+            showScreen(ekort_progress)
+        }
+
+        override fun doInBackground(vararg p0: Void?): List<ActiveECard> = eCardAPI!!.getActiveECards(0)
+
+
+        override fun onPostExecute(result: List<ActiveECard>?) {
+            if (result != null) {
+                showScreen(active_ecards)
+                active_cards_table.adapter = ActiveECardsAdapter(result, this@EkortActivity)
+                active_cards_table.layoutManager = LinearLayoutManager(this@EkortActivity)
             } else {
                 throw RuntimeException("What happened here?")
             }
